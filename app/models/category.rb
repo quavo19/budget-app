544256f97a4937ad063f0c363e151b9120e47a00
Donald Akite <donaldakite27@gmail.com>
2023-05-31 20:18:58 +0000

    Add validations and associations for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :expenditure_categories, dependent: :destroy
+    has_many :expenditures, through: :expenditure_categories
+    validates :name, presence: true, length: { minimum: 1, maximum: 25 }
+    validatable :icon, presence: true
 end
