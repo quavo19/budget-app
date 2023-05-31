94f487f61c9f2a1c0e6812bfc603e24d26ef89f0
Donald Akite <donaldakite27@gmail.com>
2023-05-31 20:18:42 +0000

    Add validations and associations

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,2 +1,7 @@
 class Expenditure < ApplicationRecord
+    belongs_to :author, :class_name: 'User'
+    has_many :expenditure_categories, dependent: :destroy
+    has_many :categories, through: :expenditure_categories
+    validates :amount, presence: true, numericality: { greater_than: 0 }
+    validates :user_id, presence: true
 end
