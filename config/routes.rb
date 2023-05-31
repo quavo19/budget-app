de731a5c040cdf4773120da0c3efb5b55579d654
Donald Akite <donaldakite27@gmail.com>
2023-05-31 19:45:09 +0000

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  resources :categories
+  resources :expenditures
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
