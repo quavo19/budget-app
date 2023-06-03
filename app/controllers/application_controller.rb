b3cf2b539a6d65a95623150fbd46a5f2aed8d4b0
Donald Akite <donaldakite27@gmail.com>
2023-06-03 11:56:05 +0000

    Fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
-end
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password])
+    end
+  end
+  
